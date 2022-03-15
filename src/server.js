ef0efd9b17281b8a519c6b531cb1ecf9e272b80d
Burnout Queen <burnout.qu33n@gmail.com>
2022-03-15 18:56:26 -0400

    woiehg

--- src/server.js
+++ src/server.js
@@ -1,24 +1,14 @@
 const { PORT = 5000 } = process.env;
-const express = require("express");
-const cors = require("cors");
+
+const app = require("./app");
 const knex = require("./db/connection");
 
-const app = express();
-const router = express.Router()
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
-router.get('/', cors(), (req, res) => {
-  res.json({ message: 'Hello Heroku!' });
-})
-
-app.use('/', router);
-
-// knex.migrate
-//   .latest()
-//   .then((migrations) => {
-//     console.log("migrations", migrations);
-//     app.listen(PORT, listener);
-//   })
-//   .catch(console.error);
-
-module.exports = app
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations);
+    app.listen(PORT, listener);
+  })
+  .catch(console.error);
